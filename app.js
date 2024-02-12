8aa9a27193f7082173c9a22049c19141791ccc9e
Shekhar_Dulgach <shekhardulgach19@gmail.com>
2024-02-12 15:19:17 +0530

    Add logic to deny user from marking on a already taken spot

--- app.js
+++ app.js
@@ -1,34 +1,21 @@
-
-
-
-
-
-// for(let clicks = 1; clicks <=9; clicks++) {
-//   const gameBoard = document.getElementsByClassName('option');
-//   if (i==1 || i==3 || i==5 || i==7 || i==9) {
-//     gameBoard[i].addEventListener('click', function () {
-//       this.innerText = 'X';
-//   })} else {
-//     gameBoard[i].addEventListener('click', function () {
-//       this.innerText = '0';
-//   })
-// }
-// }
+const user1 = [];
+const user2 = [];
 
 function startGame () {
   let clicks = 0;
   const gameBoard = document.getElementsByClassName('option');
   for ( let i = 0; i < gameBoard.length; i++) {
     gameBoard[i].addEventListener('click', function(){
-      if(clicks%2 == 0){
+      if(clicks%2 == 0 && this.innerText !== '0'){
+        user1.push(this.innerText);
         this.innerText = 'X';
-      } else {
-        this.innerText  = 'O';
+      } else if (clicks%2 !== 0 && this.innerText !== 'X'){
+        user2.push(this.innerText);
+        this.innerText  = '0';
       }
       clicks++;
     })
   }
-  
 }
 
 startGame();
